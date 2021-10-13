fc8307bf72729b4d42c9fc0ca2d5fd5c9a48824a
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-10-13 13:49:01 +0900

    ver7_4

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from config import settings
 from django.conf.urls.static import static
 
 from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
